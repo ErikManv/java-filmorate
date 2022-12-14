e5a225228b9f2f25022118376715b3fe1366d5e4
ErikManv <erikmanvelyan@yandex.ru>
2022-12-15 00:46:18 +0400

    10 commit all done

--- src/main/java/ru/yandex/practicum/filmorate/model/User.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/User.java
@@ -1,11 +1,13 @@
 package ru.yandex.practicum.filmorate.model;
 
 import lombok.Data;
+import lombok.RequiredArgsConstructor;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 
 @Data
+@RequiredArgsConstructor
 public class User {
     private int id;
     private final String login;
