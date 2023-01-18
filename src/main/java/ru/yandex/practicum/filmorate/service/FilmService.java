0d833e4f9d7ac1a9a33a67accbdd39dd34dbf298
ErikManv <erikmanvelyan@yandex.ru>
2023-01-19 00:03:43 +0400

    8 commit Все тесты Postman пройдены. Тесты написаны. Учтены комментарии.

--- src/main/java/ru/yandex/practicum/filmorate/service/FilmService.java
+++ src/main/java/ru/yandex/practicum/filmorate/service/FilmService.java
@@ -13,7 +13,6 @@ import ru.yandex.practicum.filmorate.exception.NotFoundException;
 import ru.yandex.practicum.filmorate.exception.ValidationException;
 import ru.yandex.practicum.filmorate.model.Film;
 import ru.yandex.practicum.filmorate.storage.film.FilmStorage;
-import ru.yandex.practicum.filmorate.storage.user.UserStorage;
 
 import java.time.LocalDate;
 import java.util.ArrayList;
